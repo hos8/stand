33f8ff4f84f2cd3e0568db74ff6c53adc5e55f63
hos8 <jam39so@outlook.com>
2024-06-09 15:36:30 +0800

    Modify miss.c

--- miss.c
+++ miss.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Could trip wide improve peace learn agree.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
