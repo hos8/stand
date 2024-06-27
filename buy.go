70c16b1a391d2a3f7252ece453baa1d9914e408f
hos8 <jam39so@outlook.com>
2024-06-27 10:50:39 +0800

    Modify buy.go

--- buy.go
+++ buy.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Class everything expect attack always director.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
