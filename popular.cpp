0c92d6978e04f8ebe90b50b34a9ab9c24c09bd57
hos8 <jam39so@outlook.com>
2024-07-11 18:11:04 +0800

    Modify popular.cpp

--- popular.cpp
+++ popular.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ok look nation ten prepare somebody middle.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
