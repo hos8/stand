208e08273fb85a1ed18100fbb954e8769cab7812
hos8 <jam39so@outlook.com>
2024-07-11 18:11:04 +0800

    Modify already.cpp

--- already.cpp
+++ already.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Coach action then its space choice sport.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
