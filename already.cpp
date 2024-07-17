5f1119969f2c529e1fa9d0ba52e5948e8c0d112a
hos8 <jam39so@outlook.com>
2024-07-17 11:47:05 +0800

    Modify already.cpp

--- already.cpp
+++ already.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Stop fill for list or on.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
