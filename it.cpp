6c61d834bf5b3e1815851d841066ca7a9afcded4
hos8 <jam39so@outlook.com>
2024-08-14 09:01:36 +0800

    Modify it.cpp

--- it.cpp
+++ it.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Service dog have yeah.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
