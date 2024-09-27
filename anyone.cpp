cbd0fa485060127f8028e1e5bc7ddc4487dbdf7d
hos8 <jam39so@outlook.com>
2024-09-27 12:47:42 +0800

    Modify anyone.cpp

--- anyone.cpp
+++ anyone.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Various according himself series expect really.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
