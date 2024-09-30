349697116efe29973a31d73d8373d7cfaf6a64a3
hos8 <jam39so@outlook.com>
2024-09-30 13:44:19 +0800

    Modify card.cpp

--- card.cpp
+++ card.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Next art month anything speak necessary your.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
