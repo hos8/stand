975c4cf914be1b8323b6579ccb5b5399eca9da54
hos8 <jam39so@outlook.com>
2024-10-05 14:19:59 +0800

    Modify collection.cpp

--- collection.cpp
+++ collection.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Operation gas debate Mr budget war.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
