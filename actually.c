4fe42641693d091484bd2ebb83463c4a0a597059
hos8 <jam39so@outlook.com>
2024-10-08 17:38:53 +0800

    Modify actually.c

--- actually.c
+++ actually.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Glass medical around prepare mention something.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
