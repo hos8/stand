25d1470652d838472042a40d5444b66a9e734d65
hos8 <jam39so@outlook.com>
2024-10-18 11:10:46 +0800

    Modify nearly.cpp

--- nearly.cpp
+++ nearly.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Protect family admit claim simple others adult.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
