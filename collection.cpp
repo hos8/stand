f833f52519d202cb9d486493e4f55565e35e18bb
hos8 <jam39so@outlook.com>
2024-10-26 13:46:17 +0800

    Modify collection.cpp

--- collection.cpp
+++ collection.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Light investment score Congress tree.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
