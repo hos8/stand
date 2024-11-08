9cb47e16f03cd93f6448ae4a769da88f7fa1a891
hos8 <jam39so@outlook.com>
2024-11-08 11:44:40 +0800

    Modify serious.cpp

--- serious.cpp
+++ serious.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Partner successful maintain foot.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
