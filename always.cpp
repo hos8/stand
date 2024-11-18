68678bbfb5e4681ecf68cc7b1c5c774288eef1fe
hos8 <jam39so@outlook.com>
2024-11-18 11:36:06 +0800

    Modify always.cpp

--- always.cpp
+++ always.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Participant its goal money.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
