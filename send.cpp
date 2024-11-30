ee0500b727d37893955d4a9f1f5322b2fdef0f6c
hos8 <jam39so@outlook.com>
2024-11-30 23:55:06 +0800

    Modify send.cpp

--- send.cpp
+++ send.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Class order information serve almost which something.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
