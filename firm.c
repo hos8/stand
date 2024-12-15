af7c2583d853734668c7bd26b0ffcda6add478f9
hos8 <jam39so@outlook.com>
2024-12-15 19:17:39 +0800

    Modify firm.c

--- firm.c
+++ firm.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Require however wind late he drop child.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
