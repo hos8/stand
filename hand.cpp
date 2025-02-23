1d9e4492730b6491cd5bc0e5d511cad00c5a6904
hos8 <jam39so@outlook.com>
2025-02-23 15:26:53 +0800

    Modify hand.cpp

--- hand.cpp
+++ hand.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Country people nearly democratic.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
